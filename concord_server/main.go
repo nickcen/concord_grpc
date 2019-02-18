f290e1f7673136f5ad90fce1af66fb7859a2d21a
nickcen <cenyongh@gmail.com>
2019-02-18 14:40:53 +0800

    add Delete & Init rpc service

--- concord_server/main.go
+++ concord_server/main.go
@@ -20,18 +20,19 @@ type server struct{
 }
 
 func (s *server) Get(ctx context.Context, in *pb.GetRequest) (*pb.GetReply, error) {
-  log.Printf("Received Get Request: %v", in.Key)
+  log.Printf("Received Get Request: [%v]", in.Key)
 
   val, err := s.client.Get(in.Key).Result()
   if err != nil {
-    panic(err)
+    return &pb.GetReply{Ret: true, Error: "", Value: []byte(nil)}, nil
+    // panic(err)
   }
 
-  return &pb.GetReply{Ret: true, Error: "", Value: []byte("Hello World " + val)}, nil
+  return &pb.GetReply{Ret: true, Error: "", Value: []byte(val)}, nil
 }
 
 func (s *server) Set(ctx context.Context, in *pb.SetRequest) (*pb.SetReply, error) {
-  log.Printf("Received Set Request: %v", in.Key)
+  log.Printf("Received Set Request: [%v] - [%v]", in.Key, string(in.Value))
 
   err := s.client.Set(in.Key, string(in.Value), 0).Err()
   if err != nil {
@@ -41,7 +42,30 @@ func (s *server) Set(ctx context.Context, in *pb.SetRequest) (*pb.SetReply, erro
   return &pb.SetReply{Ret: true, Error: ""}, nil
 }
 
+func (s *server) Delete(ctx context.Context, in *pb.DeleteRequest) (*pb.DeleteReply, error) {
+  log.Printf("Received Delete Request: [%v]", in.Key)
+
+  // err := s.client.Set(in.Key, string(in.Value), 0).Err()
+  // if err != nil {
+  //   panic(err)
+  // }
+
+  return &pb.DeleteReply{Ret: true, Error: ""}, nil
+}
+
+func (s *server) Init(ctx context.Context, in *pb.InitRequest) (*pb.InitReply, error) {
+  log.Print("Received Init Request")
+
+  err := s.client.FlushAll().Err()
+  if err != nil {
+    panic(err)
+  }
+
+  return &pb.InitReply{}, nil
+}
+
 func main() {
+  log.Print("listen on "+port)
   lis, err := net.Listen("tcp", port)
   if err != nil {
     log.Fatalf("failed to listen: %v", err)
