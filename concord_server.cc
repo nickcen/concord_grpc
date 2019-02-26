fa4c35c93d6bafa56dbd9b384c21fe589c0fbc03
nickcen <cenyongh@gmail.com>
2019-02-26 14:07:11 +0800

    ~

--- concord_server.cc
+++ concord_server.cc
@@ -61,8 +61,9 @@ class ConcordServiceImpl final : public Concord::Service {
     std::cout << "received Get request " << request->key() << ":" << pRedisReply->str << std::endl;
 
     std::cout << "pRedisReply len:" << pRedisReply->len << std::endl;
-
-    reply->set_value(pRedisReply->str);
+    if(pRedisReply->len > 0){
+      reply->set_value(pRedisReply->str);  
+    }
 
     freeReplyObject(pRedisReply); 
 
